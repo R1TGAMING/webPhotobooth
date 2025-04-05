922d0ccd99b0082ea08179554a3c449abad5536e
R1TGAMING <rafisofyangaming1234@gmail.com>
2025-04-05 21:04:02 +0700

    first commit

---
+++ src/components/section/Hero.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react'
+
+
+function Hero() {
+ 
+  return (
+    <div className='hero flex md:flex-row flex-col items-center justify-center my-52 '>
+      <div className='font-extrabold   text-center md:text-left '>
+        <h1 className='text-pink-400 text-6xl'>Take Your Photobooth</h1>
+        <p className='text-pink-300 text-2xl'>Ahiiii</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
